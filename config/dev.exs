50b2b13e934422d301f6470ef315fc9195d6af8f
Tim Stapleton <tim_stapleton@nikola.com>
2022-09-02 22:48:45 -0700

    add tailwind css

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :mnemosyne, MnemosyneWeb.Endpoint,
   secret_key_base: "PS+0x+uWEcAFIxMjM4bayNPLHXy3MeG2brb1RrGZ626M/9lqNpBK2rK+ZiL5cYY9",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
   ]
 
 # ## SSL Support
